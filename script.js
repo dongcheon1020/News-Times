001c1ffb2075c5eaf119faa1f39a521e993fe433
dongcheon1020 <dongcheon1020@gmail.com>
2024-02-20 23:15:26 +0900

    feat: api주소 수정

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const APT_KEY = "3458d4bb705d428295269fe64ff576b0";
 let news = [];
 const getLatestNews = async () => {
   const url = new URL(
-    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${APT_KEY}`
+    `https://https://dongcheon1020.github.io/News-Times/top-headlines?country=us&apiKey=${APT_KEY}`
   );
   const response = await fetch(url);
   const data = await response.json();
