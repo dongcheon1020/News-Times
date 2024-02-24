94e7bb63b711e034ffb35595f0e053d0aabde8f7
dongcheon1020 <dongcheon1020@gmail.com>
2024-02-24 19:00:29 +0900

    feat:pagination에 <<, >> 버튼 기능 추가

--- script.js
+++ script.js
@@ -124,7 +124,20 @@ const paginationRender = () => {
   // firstPage
   const firstPage =
     lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1);
-  let paginationHTML = ``;
+  let paginationHTML = `
+
+  <li class="page-item ${
+    page <= firstPage ? "disabled" : " "
+  } " onclick="moveToPage(${firstPage})"><a class="page-link" href="#">&lt&lt;</a></li>
+  <li class="page-item ${
+    page <= firstPage ? "disabled" : " "
+  } " onclick="moveToPage(${
+    page - 1
+  })"><a class="page-link" href="#">&lt;</a></li>
+
+
+  `;
+
   for (let i = firstPage; i <= lastPage; i++) {
     paginationHTML += `          
     <li class="page-item ${
@@ -132,6 +145,16 @@ const paginationRender = () => {
     }" onclick="moveToPage(${i})" ><a class="page-link">${i}</a></li>
   `;
   }
+  paginationHTML += `
+  <li class="page-item ${
+    page >= lastPage ? "disabled" : " "
+  } " onclick="moveToPage(${
+    page + 1
+  })"><a class="page-link" href="#">&gt;</a></li>
+  <li class="page-item ${
+    page >= lastPage ? "disabled" : " "
+  } " onclick="moveToPage(${lastPage})"><a class="page-link" href="#">&gt&gt;</a></li>
+  `;
   document.querySelector(".pagination").innerHTML = paginationHTML;
 };
 
